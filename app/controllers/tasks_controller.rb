bdd8fa728c1831bbf5c848386dc7c978cd78cee7
Norbert Csipak <norbertcipak@me.com>
2022-07-16 15:52:36 +0100

    i can add, read one, edit and delete a task from my list of tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,8 +14,23 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.save
-    # No need for app/views/restaurants/create.html.erb
-    redirect_to tasks_path(@task)
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
   end
 
   private
