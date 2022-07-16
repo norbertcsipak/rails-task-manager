25edd212a64e06b7162ed51838ee0dac4a37c7af
Norbert Csipak <norbertcipak@me.com>
2022-07-16 14:17:15 +0100

    adding a new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,4 +10,17 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    # No need for app/views/restaurants/create.html.erb
+    redirect_to tasks_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   # root "articles#index"
   get 'tasks', to: 'tasks#index'
   get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
   get 'tasks/:id', to: 'tasks#show', as: 'show_task'
 end
